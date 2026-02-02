5af9eff5125568e4fd08b8fa5fd7ea25f58a3b38
mosuumo <t125052@ed.sus.ac.jp>
2026-02-03 05:35:22 +0900

    3rd

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-df1 = pd.read_csv("第1表.csv")
-df2 = pd.read_csv("第2表.csv")
-df3 = pd.read_csv("第3表.csv")
+df = pd.read_csv("第1表.csv", header=[0,1],)
+
+st.header('食中毒の原因食品と発生')
 
 with st.sidebar:
-    options = ["原因食品", "病因物質", "原因施設"]
-    select = st.segmented_control(
-        "カテゴリの選択", options, selection_mode="single",
-    )
-    prefecture = st.multiselect('都道府県を選択してください',
-                                    df1['都道府県'])
+    prefecture = st.multiselect('地域を選択してください',
+                                    df[df.columns[0]])
+    prime_category = st.radio('')
 
-if select == "原因食品":
-    #with st.sidebar:
-    if prefecture == []:
-        st.dataframe(df1, width=800, height=500)    
-    else:
-        df1 = df1[df1['都道府県'].isin(prefecture)]
-        df1.set_index('都道府県', inplace=True)
-        st.dataframe(df1, width=800)
+#with st.sidebar:
+if prefecture == []:
+    st.subheader('食中毒の原因食品と発生の詳細の表')
+    df.set_index('地域', inplace=True)
+    st.dataframe(
+        df , width=800, height=500
+    )    
+else:
+    df = df[df[df.columns[0]].isin(prefecture)]
+    df.set_index('地域', inplace=True)
+    st.dataframe(df, width=800)
