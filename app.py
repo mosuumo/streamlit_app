5ea25c03a269a1e9e4a9c8a893633f0f903863a5
mosuumo <t125052@ed.sus.ac.jp>
2026-02-03 13:51:23 +0900

    8th

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ st.set_page_config(
 import pandas as pd
 
 df = pd.read_csv("file.csv", header=[0,1],)
-category = df.iloc[0].unique()
 
 st.header('食中毒の原因食品と地域ごとの比較')
 
@@ -14,7 +13,7 @@ with st.sidebar:
                                     df[df.columns[0]])
     prime_category = st.multiselect('カテゴリーを選択してください', df.columns.get_level_values(0).unique()[1:])
     with st.expander('出典'):
-        ('https://www.e-stat.go.jp/stat-search/files?page=1&toukei=00450191&tstat=000001040259&file_type=1&result_page=1&metadata=1&data=1, 2026/2/2取得)')
+        st.write('https://www.e-stat.go.jp/stat-search/files?page=1&toukei=00450191&tstat=000001040259&file_type=1&result_page=1&metadata=1&data=1, 2026/2/2取得)')
 
 
 st.subheader('サイドバーで地域、カテゴリーの選択をし、食中毒と地域ごとの発生回数を知り、比較することができるアプリです')
